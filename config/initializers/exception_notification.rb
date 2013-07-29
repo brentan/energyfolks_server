e0ad0ea55f48a659a795edd511e94cea80329123
brentanalexander <brentan@energysociety.org>
2013-07-28 21:45:34 -0700

    Setup EC2

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,10 +1,10 @@
 EXCEPTION_PREFIX = SITE_SPECIFIC['exception_notifier']['email_prefix']
 EXCEPTION_RECIPIENTS = SITE_SPECIFIC['exception_notifier']['exception_recipients']
 EXCEPTION_SENDER = SITE_SPECIFIC['exception_notifier']['sender_address']
-
-if Rails.env.to_s != 'development'
-  EnergyfolksServer::Application.config.middleware.use ExceptionNotifier,
-		:email_prefix => EXCEPTION_PREFIX,
-		:sender_address => EXCEPTION_SENDER,
-		:exception_recipients => EXCEPTION_RECIPIENTS.split(' ')
-end
+# TODO: FIND A FIX TO LET THIS WORK IN AWS EB
+#if Rails.env.to_s != 'development'
+#  EnergyfolksServer::Application.config.middleware.use ExceptionNotifier,
+#		:email_prefix => EXCEPTION_PREFIX,
+#		:sender_address => EXCEPTION_SENDER,
+#		:exception_recipients => EXCEPTION_RECIPIENTS.split(' ')
+#end
