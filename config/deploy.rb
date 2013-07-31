949881f8f94733069676e12583fe42ffbb47c7fb
brentanalexander <brentan@energysociety.org>
2013-07-30 17:50:56 -0700

    Setup EC2

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,8 +9,9 @@
 # 2. $ cap production
 
 require 'bundler/capistrano'
-require "delayed/recipes"
-require "whenever/capistrano"
+require 'delayed/recipes'
+require 'whenever/capistrano'
+require 'capify-ec2/capistrano'
 
 default_run_options[:pty] = true  # Must be set for the password prompt from git to work
 set :repository, "git@github.com:brentan/energyfolks_server.git"
@@ -18,6 +19,9 @@ set :scm, "git"
 set :deploy_via, :remote_cache
 set :domain, 'ec2-54-215-158-64.us-west-1.compute.amazonaws.com'
 
+ec2_roles :name=>"web", :options => {:default=>true}
+ec2_roles :name=>"app", :options => {:default=>true}
+ec2_roles :name=>"db", :options => {:default=>true}
 
 role :web, domain # Your HTTP server, Apache/etc
 role :app, domain # This may be the same as your `Web` server
@@ -53,4 +57,9 @@ after "deploy:migrate", :setup_group
 task :setup_group do
   run "sudo chgrp -R webapp #{deploy_to}/releases"#" && chmod -R g+s #{deploy_to}"
   run 'sudo /etc/init.d/passenger restart'
+end
+
+after "deploy:assets:symlink" do
+  run "ln -nfs #{shared_path}/config/whenever-elasticbeanstalk.yml #{release_path}/config/whenever-elasticbeanstalk.yml"
+  run "ln -nfs #{shared_path}/config/site.yml #{release_path}/config/site.yml"
 end
