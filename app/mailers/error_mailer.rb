34ad5fced65fa7430bb31f831a04f799299f960f
brentanalexander <brentan@energysociety.org>
2013-07-30 22:43:40 -0700

    Fix error sending bugs

--- app/mailers/error_mailer.rb
+++ app/mailers/error_mailer.rb
@@ -1,6 +1,9 @@
 class ErrorMailer < ActionMailer::Base
-  def experror(e)
+  def experror(e, u, a, r)
     @err=e
+    @user = u
+    @aff = a
+    @req = r
     mail(:to => EXCEPTION_RECIPIENTS,
         :from => EXCEPTION_SENDER,
         :subject => "#{EXCEPTION_PREFIX}#{e.message}")
