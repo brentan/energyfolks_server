8db311872c066e6ddb7f64f04ea993862a04a0d3
brentanalexander <brentan@energysociety.org>
2013-08-22 21:31:42 -0700

    ec2 test

--- db/schema.rb
+++ db/schema.rb
@@ -192,6 +192,8 @@ ActiveRecord::Schema.define(:version => 20130821210320) do
     t.integer "count", :default => 0
   end
 
+  add_index "tags", ["name"], :name => "index_tags_on_name"
+
   create_table "tags_entities", :force => true do |t|
     t.integer "entity_id"
     t.string  "entity_type"
