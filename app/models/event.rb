030adcdc635079f8281c69d49acc6ea997f938d8
brentanalexander <brentan@energysociety.org>
2013-10-29 18:41:27 -0700

    finalize uploads to s3

--- app/models/event.rb
+++ app/models/event.rb
@@ -20,7 +20,7 @@ class Event < ActiveRecord::Base
 
   has_attached_file :logo, {
       :styles => { :medium => "120x200>", :thumb_big => "90x90#", :thumb => "40x40#" },
-      :path => "#{Rails.root}/public/system/logos_events/:hash.:extension",
+      :path => Rails.env != 'production' ? Paperclip::Attachment.default_options[:path] : "//logos_events/:hash.:extension",
       :hash_secret => "asfAdsfmasdfaSDFj23enujdskfsdjkfn23unjasdkfnakjsdfnnff-"
   }
   validates_attachment :logo,
