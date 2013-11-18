54b42439e326133e041a8ba19b1e9b0290f1da29
brentanalexander <brentan@energysociety.org>
2013-11-18 15:33:28 -0800

    fix irreversible migration to allow rollbacks

--- db/migrate/20131118174931_create_comment_subscriber.rb
+++ db/migrate/20131118174931_create_comment_subscriber.rb
@@ -1,16 +1,21 @@
 class CreateCommentSubscriber < ActiveRecord::Migration
-  def change
-    create_table :comment_subscribers do |t|
-      t.integer :user_id
-      t.string :comment_hash
-    end
-    add_index :comment_subscribers, :comment_hash
-    create_table :comment_details do |t|
-      t.string :name
-      t.string :url
-      t.string :comment_hash
-    end
-    add_index :comment_details, :comment_hash
-    remove_column :comments, :url
+  def up
+   create_table :comment_subscribers do |t|
+     t.integer :user_id
+     t.string :comment_hash
+   end
+   add_index :comment_subscribers, :comment_hash
+   create_table :comment_details do |t|
+     t.string :name
+     t.string :url
+     t.string :comment_hash
+   end
+   add_index :comment_details, :comment_hash
+   remove_column :comments, :url
+  end
+  def down
+    drop_table :comment_subscribers
+    drop_table :comment_details
+    add_column :comments, :url,	:string
   end
 end
