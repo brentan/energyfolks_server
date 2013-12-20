7036135dd5ac83236c828025b5bcff51638ce590
Joe Westersund <joe_westersund@hotmail.com>
2013-12-19 23:54:18 -0800

    edit users/edit to have a menu bar on the left.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -52,23 +52,24 @@ module ApplicationHelper
     link_to text, '#', class: (delete ? 'EnergyFolks_delete' : 'EnergyFolks_ajax'), data: {command: command, params: params.to_query}
   end
 
-  def carousel_slide_title_section(title_text, slide_count)
-    html = "<div class='carousel_title_section'><h1>#{title_text}</h1>"
-    html += progress_bar(slide_count)
-    html += "<hr></div>"
+  def carousel_slide_title_section(title_text, slide_count, options = {})
+    html = "<div class='carousel_title_section#{" show_vertical" if options[:show_vertical] }'><h1>#{title_text}</h1>"
+    html += progress_bar(slide_count, options[:slide_titles])
+    html += "<hr>" unless options[:show_vertical]
+    html += "</div>"
   end
 
-	def progress_bar(number_of_steps)
+	def progress_bar(number_of_steps, slide_titles)
 		html = ''
 		html += "<div class='progress_bar pager'>"
 		number_of_steps.times do |index|
 			step = index + 1  #index is zero-based, we want steps to be numbered from 1
-      #if step == current_step
-      #  html += "<a rel='#{index}' href='#' class='pagenum current_page'>#{step}</a>"
-      #else
+      if slide_titles.nil? || slide_titles[index].nil?
         html += "<a rel='#{index}' href='#' class='pagenum'>#{step}</a>"
-      #end
-		end
+      else
+        html += "<a rel='#{index}' href='#' class='pagenum'>#{slide_titles[index]}</a>"
+      end
+    end
 	  html += '</div>'
   end
 
