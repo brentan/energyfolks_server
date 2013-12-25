b9ed4457fa1e17cdd266654b39026fd024354bf5
brentanalexander <brentan@energysociety.org>
2013-12-25 11:17:50 -0800

    update to https://www

--- public/wordpress/plugin_source/SettingsPage.php
+++ public/wordpress/plugin_source/SettingsPage.php
@@ -1,8 +1,8 @@
 
 <div class="wrap" style="padding-top: 10px;">
 <?php if (get_option('energyfolks_plugin_enabled') != '1') { ?>
-    <h1>Synchronize with EnergyFolks</h1>Your plugin installation is incomplete!  <a href='http://server.energyfolks.com/developers/wordpress_sync?return_url=<?php echo urlencode(get_site_url()); ?>'>Synchronize your plugin with energyfolks</a>.
+    <h1>Synchronize with EnergyFolks</h1>Your plugin installation is incomplete!  <a href='https://www.energyfolks.com/developers/wordpress_sync?return_url=<?php echo urlencode(get_site_url()); ?>'>Synchronize your plugin with energyfolks</a>.
 <?php } else { ?>
-    <iframe src="http://server.energyfolks.com/affiliates/dashboard?aid=<?php echo get_option('energyfolks_affiliate_id'); ?>&iframe_next=true" width="1035" height="1000" frameborder="0" border="0"></iframe>
+    <iframe src="https://www.energyfolks.com/affiliates/dashboard?aid=<?php echo get_option('energyfolks_affiliate_id'); ?>&iframe_next=true" width="1035" height="1000" frameborder="0" border="0"></iframe>
     <?php } ?>
 </div>
