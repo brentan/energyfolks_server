5dfb121eb90bc605de019fd7cad092a4b721fab3
brentanalexander <brentan@energysociety.org>
2014-02-06 17:49:46 -0800

    map display fix

--- vendor/assets/javascripts/energyfolks/data_display.js
+++ vendor/assets/javascripts/energyfolks/data_display.js
@@ -383,7 +383,7 @@ EnergyFolks.showMap = function() {
     else
         EnergyFolks.$('#EnfolksResultDiv').html("<div id='EnfolksMapDiv'><div id='EnfolksMapDiv_map'></div><div id='EnfolksMapDiv_loading'></div></div>");
     if((EnergyFolks.map_lat == 0) && (EnergyFolks.map_lng == 0)) {
-        window.setTimeout(function() {if(EnergyFolks.showMap()) EnergyFolks.loadData(); }, 250);
+        window.setTimeout(function() {if(EnergyFolks.showMap()) window.setTimeout(function() {EnergyFolks.loadData(); }, 50)}, 250);
         return false;
     }
     EnergyFolks.map_layer = EnergyFolks.Leaflet.map('EnfolksMapDiv_map').setView([EnergyFolks.map_lat, EnergyFolks.map_lng], EnergyFolks.map_zoom);
