8d4bcccf347207433d9c2008e4f706cdcb0af831
brentan <brentan@energyfolks.com>
2014-02-21 09:53:20 -0800

    start salesfoce

--- db/schema.rb
+++ db/schema.rb
@@ -517,8 +517,6 @@ ActiveRecord::Schema.define(:version => 20140205220432) do
     t.integer "count", :default => 0
   end
 
-  add_index "tags", ["name"], :name => "index_tags_on_name"
-
   create_table "tags_entities", :force => true do |t|
     t.integer "entity_id"
     t.string  "entity_type"
